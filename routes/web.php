5e12380166f08bddca52d81dbca26bd65ca04fb9
ReynanddXD <reynandaap@gmail.com>
2025-09-28 03:54:03 +0700

    role model user

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,46 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth; 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/dashboard', function () {
+    $user = Auth::user();
+
+    if ($user->role === 'admin') {
+        return redirect()->route('admin.dashboard');
+    }
+
+    return redirect()->route('landing.page');
+})->middleware('auth');
+
+Route::get('/landing-page',function(){
+    return view('halaman.landingPage');
+})->name('landing.page');
+
+Route::get('/admin-dashboard', function () {
+    return view('layouts.adminLayouts');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('admin.dashboard'); 
+
+
+Route::get('/dashboard/admin/webinar',function(){
+    return view('halaman.dashboardAdminWebinar');
+})->middleware(['auth','verified','RoleCheck:admin'])->name('halaman.dashboardAdminWebinar');
+
+// Route::get(/product', [ProductController::class, 'index']);
 
 require __DIR__.'/auth.php';
 
