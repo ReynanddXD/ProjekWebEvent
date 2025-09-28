5976fe0afed7efad9ce98eb034f7806d13fac1ed
Serroria <merylien.jovanda14@gmail.com>
2025-09-28 19:11:25 +0700

    dinamis card hal User

--- app/Http/Controllers/DashboarduserController.php
+++ app/Http/Controllers/DashboarduserController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Lomba;
 
 use Illuminate\Http\Request;
 
 class DashboarduserController extends Controller
 {
     public function index (){
-        return view('halaman.dashboarduser');
+         $semuaLomba = Lomba::latest()->get();
+        return view('halaman.dashboarduser', compact('semuaLomba'));
 }
 }
