200ccc1dcbda591efcc70cc473789fd581dc6e4d
ReynanddXD <reynandaap@gmail.com>
2025-09-29 23:20:28 +0700

    fix routing

--- routes/web.php
+++ routes/web.php
@@ -19,45 +19,6 @@ Route::get('/main', function () {
 });
 
 Route::get('/dashboard-user',  [DashboarduserController::class, 'index']);
-// Route::view('/navbar',  view:'partial.navbar');
-// Route::view('/footer',  view:'partial.footer');
-// Route::get('/dashboarduser', [DashboarduserController::class, 'index'])
-//     ->middleware('auth')
-//     ->name('halaman.dashboarduser');
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// Route::get('/dashboard', function () {
-//  $user = Auth::user();
-
-//     if ($user->role === 'admin') {
-//         return redirect()->route('admin.dashboard');
-//     }
-
-//     return redirect()->route('landing.page');
-// })->middleware('auth');
-
-// Route::get('/dashboard', function(){
-// return view('halaman.dashboard');
-// });
-
-Route::get('/dashboard', function () {
-    $user = Auth::user();
-
-    if ($user->role === 'admin') {
-        return redirect()->route('halaman.dashboard');
-    }
-
-    return redirect()->route('landingPage');
-})->middleware('auth');
 
 Route::get('/landing-page',function(){
     return view('halaman.landingPage');
@@ -67,47 +28,29 @@ Route::get('/admin/dashboard', function () {
     return view('halaman.dashboard');
 })->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('halaman.dashboard');
 
-// Route::get('/dashboard/admin/webinar',function(){
-//     return view('halaman.dashboardAdminWebinar');
-// })->middleware(['auth','verified','RoleCheck:admin'])->name('halaman.dashboardAdminWebinar');
-
 Route::get('/dashboard/admin/webinar',function(){
     return view('halaman.dashboardAdminWebinar');
-});
-
-// Route::get(/product', [ProductController::class, 'index']);
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('halaman.dashboardAdminWebinar');;
 
 require __DIR__.'/auth.php';
 
-// Route::get('admin', function(){
-//     return view('layouts.adminLayouts');
-// });
-
-
 Route::get('/admin', function(){
     return view('halaman.dashboard');
-})->name('halaman.dashboard');
-
-// Route::get('/admin/lomba', function(){
-//     return view('halaman.dashboardAdminLomba');
-// })->name('halaman.dashboardAdminLomba');
-
-// Route::get('/admin/webinar', function(){
-//     return view('halaman.dashboardAdminWebinar');
-// })->name('halaman.dashboardAdminWebinar');
+})->middleware(['auth', 'verified', 'RoleCheck:admin'])->name('halaman.dashboard');
 
 //menampilkan isi form
-Route::get('/admin/lomba', [LombaController::class, 'create'])->name('lomba.create');
-Route::post('/lomba',[LombaController::class, 'store'])->name('lomba.store');
+// Lomba (admin only)
+Route::middleware(['auth', 'verified', 'RoleCheck:admin'])->group(function () {
+    Route::get('/admin/lomba', [LombaController::class, 'create'])->name('lomba.create');
+    Route::post('/lomba', [LombaController::class, 'store'])->name('lomba.store');
+    Route::get('/lomba/tabelLomba', [LombaController::class, 'index'])->name('lomba.index');
+    Route::get('/lomba/tabel', [LombaController::class, 'index'])->name('lomba.index');
 
-
-Route::get('lomba/tabelLomba', [LombaController::class, 'index'])->name('lomba.index');
-
-Route::get('lomba/tabel', [LombaController::class, 'index'])->name('lomba.index');
-Route::get('/admin/webinar', [WebinarController::class, 'create'])->name('webinar.create');
-Route::post('/webinar', [WebinarController::class, 'store'])->name('webinar.store');
-
-Route::get('/admin/tabelWebinar', [WebinarController::class, 'index'])->name('webinar.index');
+    // Webinar (admin only)
+    Route::get('/admin/webinar', [WebinarController::class, 'create'])->name('webinar.create');
+    Route::post('/webinar', [WebinarController::class, 'store'])->name('webinar.store');
+    Route::get('/admin/tabelWebinar', [WebinarController::class, 'index'])->name('webinar.index');
+});
 
 Route::get('/cek-php', function () {
     phpinfo();
@@ -125,20 +68,26 @@ Route::get('/check-limits', function() {
 
 // narik data lomba di landing page
 Route::get('/', [LandingPageController::class, 'index'])->name('landingPage');
+
 // route untuk dashboard user
 Route::get('/dashboardUser', function () {
     return view('halaman.dashboardUser');
-})->name('dashboardUser');
+})->middleware(['auth', 'verified', 'RoleCheck:user, admin'])->name('dashboardUser');
+
+Route::middleware(['auth', 'verified', 'RoleCheck:user'])->group(function () {
 
 // agar dahsboard user bisa narik data lomba dan webinar
 Route::get('/dashboardUser', [DashboardUserController::class, 'index'])->name('dashboardUser');
 
+// route lomba dan webinar user
 Route::get('/user/lomba', [LombaUser::class, 'create'])->name('ulomba.create');
 Route::post('/user/lomba/daftar', [LombaUser::class, 'store'])->name('ulomba.store');
 Route::get('/user/lomba/tampil', [LombaUser::class, 'index'])->name('ulomba.index');
-
 Route::get('/user/webinar', [WebinarUser::class, 'create'])->name('uwebinar.create');
 Route::post('/user/webinar/daftar', [WebinarUser::class, 'store'])->name('uwebinar.store');
+});
+
+
 
 
 
